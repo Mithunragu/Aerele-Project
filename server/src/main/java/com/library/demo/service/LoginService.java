bcb0d1191933d3a8e2e736a028a680d9d3223d3b
mithunragu <mithunchakravarthi256@gmail.com>
2023-09-30 08:02:51 +0530

    Deleted

--- server/src/main/java/com/library/demo/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.library.demo.service;
-import com.library.demo.model.LoginModel;
-import com.library.demo.model.Responses;
-
-public interface LoginService {
-
-	public Responses login(LoginModel result);
-
-}
